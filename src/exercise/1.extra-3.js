311a47d10deea454ce555036a3ae77389e708f30
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 19:02:56 +0000

    refactor: uses utils and PokemonInfoFallback - closes #1.3

--- src/exercise/1.extra-3.js
+++ src/exercise/1.extra-3.js
@@ -2,31 +2,13 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
-
-
-function createResource(promise) {
-  let status = 'pending'
-  let result = promise.then(
-    resolved => {
-      status = 'success'
-      result = resolved
-    },
-    rejected => {
-      status = 'error'
-      result = rejected
-    },
-  )
-
-  return {
-    read() {
-      if (status === 'pending') throw result
-      if (status === 'error') throw result
-      if (status === 'success') return result
-      throw new Error('Unable to fetch pokemon')
-    },
-  }
-}
+import {
+  fetchPokemon,
+  PokemonDataView,
+  PokemonErrorBoundary,
+  PokemonInfoFallback,
+} from '../pokemon'
+import { createResource } from '../utils'
 
 let pokemonResource = createResource(fetchPokemon('pikachu'))
 
@@ -42,16 +24,12 @@ function PokemonInfo() {
   )
 }
 
-function LoadingPokemon() {
-  return <div>loading pokemon...</div>
-}
-
 function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         <PokemonErrorBoundary>
-          <React.Suspense fallback={<LoadingPokemon />}>
+          <React.Suspense fallback={<PokemonInfoFallback />}>
             <PokemonInfo />
           </React.Suspense>
         </PokemonErrorBoundary>
