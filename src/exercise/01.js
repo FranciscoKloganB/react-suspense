ec03d05811aba259a00117721a3c6265142f7277
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 18:32:03 +0000

    feat: add error boundary on failed fetch - closes #1.1

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,17 +4,23 @@
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
 // 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-import {fetchPokemon, PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
 
 // 🐨 create a variable called "pokemon" (using let)
 // 🐨 assign a pokemonPromise to a call to fetchPokemon('pikachu') with render-as-you-fetch approach!
 // 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
 let pokemon
-let pokemonPromise = fetchPokemon('pikachu').then(data => pokemon = data)
+let pokemonError
+let pokemonPromise = fetchPokemon('pikachu')
+  .then(data => (pokemon = data))
+  .catch(error => (pokemonError = error))
 
 function PokemonInfo() {
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (pokemonError) {
+    throw pokemonError
+  }
   if (!pokemon) {
     throw pokemonPromise
   }
@@ -30,8 +36,8 @@ function PokemonInfo() {
   )
 }
 
-function LoadingPokemon({name = 'Pikachu'} = {}) {
-  return <div>loading pokemon: {name}...</div>
+function LoadingPokemon() {
+  return <div>loading pokemon...</div>
 }
 
 function App() {
@@ -39,9 +45,11 @@ function App() {
     <div className="pokemon-info-app">
       <div className="pokemon-info">
         {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <React.Suspense fallback={<LoadingPokemon />}>
-          <PokemonInfo />
-        </React.Suspense>
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<LoadingPokemon />}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
       </div>
     </div>
   )
