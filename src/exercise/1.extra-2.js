f4d3fad166f3d5b04dbddf13ba269738013d85d0
Francisco Barros <francisco.teixeira.de.barros@outlook.com>
2022-03-20 18:38:01 +0000

    chore: clone exercise 1, starts #1.2

---
+++ src/exercise/1.extra-2.js
@@ -0,0 +1,49 @@
+// Simple Data-fetching
+// http://localhost:3000/isolated/exercise/01.js
+
+import * as React from 'react'
+import {fetchPokemon, PokemonDataView, PokemonErrorBoundary} from '../pokemon'
+
+let pokemon
+let pokemonError
+let pokemonPromise = fetchPokemon('pikachu')
+  .then(data => (pokemon = data))
+  .catch(error => (pokemonError = error))
+
+function PokemonInfo() {
+  if (pokemonError) {
+    throw pokemonError
+  }
+  if (!pokemon) {
+    throw pokemonPromise
+  }
+
+  return (
+    <div>
+      <div className="pokemon-info__img-wrapper">
+        <img src={pokemon.image} alt={pokemon.name} />
+      </div>
+      <PokemonDataView pokemon={pokemon} />
+    </div>
+  )
+}
+
+function LoadingPokemon() {
+  return <div>loading pokemon...</div>
+}
+
+function App() {
+  return (
+    <div className="pokemon-info-app">
+      <div className="pokemon-info">
+        <PokemonErrorBoundary>
+          <React.Suspense fallback={<LoadingPokemon />}>
+            <PokemonInfo />
+          </React.Suspense>
+        </PokemonErrorBoundary>
+      </div>
+    </div>
+  )
+}
+
+export default App
