25535edfb3e3f3520800b71db8f777b21df9e823
FranciscoKloganB <francisco.teixeira.de.barros@outlook.com>
2022-03-22 22:39:51 +0000

    feat: add cache expiration to pokemon resources - closes #4.3

--- src/exercise/context/pokemon-cache.context.js
+++ src/exercise/context/pokemon-cache.context.js
@@ -12,25 +12,31 @@ export function usePokemonResourceCache() {
   return React.useContext(PokemonResourceCacheContext)
 }
 
-export function PokemonResourceCacheProvider({children}) {
+export function PokemonResourceCacheProvider({children, cacheTimeMs = 10000}) {
   // By using React.useRef({}) we avoid creating a new object every render
   // We want to keep the object consistent through renders - same reference!
   const cache = React.useRef({})
+  const expirations = React.useRef({})
 
   // We want to make getPokemonResourceFromCache stable, so we memoize it on useCallback
+  // This allows us to avoid re-renders when by using this function in dependency
+  // array of consumers.
   const getPokemonResourceFromCache = React.useCallback(pokemonName => {
     let pokemonNameLowered = pokemonName.toLowerCase()
     let pokemonResource = cache.current[pokemonNameLowered]
+    let expiresAt = expirations.current[pokemonName] || 0
 
-    if (pokemonResource) {
+    if (pokemonResource && expiresAt > Date.now()) {
       return pokemonResource
     }
 
     pokemonResource = createPokemonResource(pokemonNameLowered)
+
     cache.current[pokemonNameLowered] = pokemonResource
+    expirations.current[pokemonNameLowered] = Date.now() + cacheTimeMs
 
     return pokemonResource
-  }, [])
+  }, [cacheTimeMs])
 
   return (
     <PokemonResourceCacheContext.Provider value={getPokemonResourceFromCache}>
